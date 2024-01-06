f4d991d68203bc1ccf73a42385f46ba51bf5590d
KANAGALA VENKATA NAGA RAJASEKHAR <109467292+kvnrajasekhar@users.noreply.github.com>
2024-01-06 22:51:01 +0530

    Create set matrix zeros.java

---
+++ set matrix zeros.java
@@ -0,0 +1,25 @@
+// link : https://leetcode.com/problems/set-matrix-zeroes/
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[]  row = new boolean[m];
+        boolean[]  col = new boolean[n];
+        for(int r=0;r<m;r++){
+            for(int c=0;c<n;c++){
+                if(matrix[r][c]==0){
+                    row[r]=true;
+                    col[c]=true;
+                }
+            }
+        }
+         for(int r=0;r<m;r++){
+            for(int c=0;c<n;c++){
+                if(row[r] || col[c]==true){
+                    matrix[r][c]=0;
+                }
+            }
+         }
+    }
+}
