4ae2bbcff021a8b995dc202824da0874b9bc48d3
KANAGALA VENKATA NAGA RAJASEKHAR <109467292+kvnrajasekhar@users.noreply.github.com>
2024-05-11 17:20:36 +0530

    Update Best Time to Buy and Sell Stock.py

--- Best Time to Buy and Sell Stock.py
+++ Best Time to Buy and Sell Stock.py
@@ -13,4 +13,22 @@ class Solution:
             buy=min(buy,i)
             prof=max(prof,i-buy)
         return prof
+
+
+# optimized solution
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
         
+        min_price = float('inf')
+        max_profit = 0
+        
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+        
+        return max_profit
+
