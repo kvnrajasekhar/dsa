f46d29435930d037118e451a9c7dc56136bc1f9e
KANAGALA VENKATA NAGA RAJASEKHAR <109467292+kvnrajasekhar@users.noreply.github.com>
2024-05-13 23:28:44 +0530

    Create Minimum Cost to Hire K Workers.py

---
+++ Minimum Cost to Hire K Workers.py
@@ -0,0 +1,22 @@
+# Link:  https://leetcode.com/problems/largest-local-values-in-a-matrix/
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        res = float("inf")
+        pairs = [] # (ratio, quality)
+        for i in range(len(quality)):
+            pairs.append((wage[i] / quality[i], quality[i]))
+        pairs.sort(key=lambda p:p[0])
+        maxHeap = [] # Qualities
+        total_quality = 0
+        for rate, q in pairs:
+            heapq.heappush(maxHeap, -q)
+            total_quality += q
+            if len(maxHeap) > k:
+                total_quality += heapq.heappop (maxHeap)
+            if len(maxHeap) == k:
+                res = min(
+                        res,
+                        total_quality * rate
+                        )
+        return res
+        
